a2c6036810750edfc3e286f3e247fec81d1c67cf
manuthkausilu <manuthkausilu20031018@gmail.com>
2026-03-30 06:58:44 +0530

    add configuration files for Order-Service and Product-Service

--- src/test/java/lk/ijse/eca/customerservice/CustomerServiceApplicationTests.java
+++ src/test/java/lk/ijse/eca/customerservice/CustomerServiceApplicationTests.java
@@ -2,9 +2,13 @@ package lk.ijse.eca.customerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import lk.ijse.eca.customerservice.repository.CustomerRepository;
 
 @SpringBootTest
 class CustomerServiceApplicationTests {
+	@MockitoBean
+	private CustomerRepository customerRepository;
 
 	@Test
 	void contextLoads() {
